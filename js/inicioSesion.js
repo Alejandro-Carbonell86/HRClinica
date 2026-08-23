03f1f3417105f880eec06d82a47dc463ce60abc4
Alejandro-Carbonell86 <alekarbonell@gmail.com>
2026-08-23 00:04:24 -0300

    inicioSesion.php y js cradors

---
+++ js/inicioSesion.js
@@ -0,0 +1,20 @@
+const formulario = document.querySelector('#inicio');
+
+formulario.addEventListener('submir', async (e)=> {
+    
+    e.preventDefault();
+
+    const datosI = new FormData();
+    datosI.append('usuario', formulario.usuario.value);
+    datosI.append('contrasenia', formulario.contrasenia.value);
+
+    const respuesta = await fetch ('../php/inicioSesion.php', {
+        method: 'POST',
+        body: datosI
+    });
+
+    const mensaje = await respuesta.text();
+    if (mensaje.trim() === "error"){
+        alert('Usuario o contraseña incorrecta');
+    }
+})
---
+++ php/inicioSesion.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'conexion.php';
+
+$usuario = $_POST['usuario'];
+$contrasenia = $_POST['contrasenia'];
+
+$stmt = $con->prepare("SELECT usuario, password_hash FROM usuarios_sistema WHERE usuario = ?, password_hash = ?");
+$stmt->bind_param('ss', $usuario, $contrasenia);
+$stmt->execute();
+
+$resultado = $stmt->get_result();
+
+if($resultado->num_rows > 0){
+    header('Location: ../paginas/dashboard.html');
+    exit;
+}else{
+    echo "error";
+}
+
+$stmt->close();
+$con->close();
+?>
