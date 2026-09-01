90fa69b38a8885734057340a706a0a49b7989ee0
Alejandro-Carbonell86 <alekarbonell@gmail.com>
2026-08-31 23:48:28 -0300

    actulización de dashboars.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,6 +2,5 @@ const traslados = document.getElementById('traslados');
 const documentos = document.getElementById('documentos');
 
 traslados.addEventListener('click', ()=> {
-
-
+documentos.style.visibility = "visible";
 })
