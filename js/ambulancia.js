0aed7dec3585a2126efe3d3a2050cb018510646e
Alejandro-Carbonell86 <alekarbonell@gmail.com>
2026-09-08 23:12:16 -0300

    registro de ambulancia realizado

---
+++ js/ambulancia.js
@@ -0,0 +1,28 @@
+const formulario = document.querySelector('#ambulancia');
+
+formulario.addEventListener('submit', async(e)=> {
+    e.preventDefault();
+
+    const ambulancia = new FormData();
+    ambulancia.append('matricula', formulario.matricula.value);
+    ambulancia.append('modelo', formulario.modelo.value);
+    ambulancia.append('marca', formulario.marca.value);
+    ambulancia.append('ano', formulario.ano.value);
+    ambulancia.append('tipo', formulario.tipo.value);
+    ambulancia.append('capacidad', formulario.capacidad.value);
+    ambulancia.append('fecha', formulario.fecha.value);
+
+    const respuesta = await fetch('../php/ambulancias.php', {
+        method: 'POST',
+        body: ambulancia
+    })
+
+    let mensaje = await respuesta.json();
+    console.log('Respuesta: ', mensaje)
+
+    if(mensaje.estado){
+        alert('Ambulancia Guardada Correctamente');
+    }else{
+        alert('Error al guardar ambulancia');
+    }
+})
---
+++ php/ambulancias.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'conexion.php';
+
+
+$matricula = $_POST['matricula'];
+$marca =  $_POST['marca'];
+$modelo = $_POST['modelo'];
+$ano = $_POST['ano'];
+$tipo = $_POST['tipo'];
+$capacidad = $_POST['capacidad'];
+$fecha = $_POST['fecha'];
+$activo = 1;
+
+
+$stmt = $con->prepare("INSERT INTO ambulancias (matricula, marca, modelo, ano, tipo, capacidad, 
+fecha_ultimo_mantenimiento, activo) VALUES (?,?,?,?,?,?,?,?)");
+$stmt->bind_param('sssisisi', $matricula, $marca, $modelo, $ano, $tipo, $capacidad, $fecha, $activo);
+
+header('Content-Type: application/json');
+
+if($stmt->execute()){
+    echo json_encode(['estado'=> true]);
+}else{
+    echo json_encode(['estado'=> false]);
+}
+
+$stmt->close();
+$con->close();
+?>
